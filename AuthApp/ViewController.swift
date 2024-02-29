de16d80938f2ecfa2aa150320f59efb4f8485bf3
VurdiOS <123585026+VurdIOS@users.noreply.github.com>
2024-02-29 20:32:44 +0300

    Initial Commit

---
+++ AuthApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AuthApp
+//
+//  Created by Камаль Атавалиев on 29.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
